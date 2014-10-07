e187de46d2abd1fb0a1129976bc1baca63c37d45
John Crocker <johnniecrocker@yahoo.com>
2014-10-07 14:12:28 -0400

    can console log image url

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,34 @@
 console.log('The Iron Yard Rocks');
 
 
-var cont = ('#mainbox');
+var cont = document.getElementById('mainbox');
 
 var pricearray = items.forEach(function(items){
 
                                           console.log(items)
+                                          console.log(items.results.length)
+
 
                                         });
+var results = (25)
+
+var imageurl = items.forEach(function(items){
+
+                        items.results.forEach(function(results) {
+
+                            results.Images.forEach(function (images){
+
+                              console.log(images.url_170x135);
+                              return ('images.url_170x135');
+
+                              })
+                            })
+                          });
+
+
+
+items.forEach (function(items){
+
+      cont.text('<li>' + results  + '<li>');
+
+    });
