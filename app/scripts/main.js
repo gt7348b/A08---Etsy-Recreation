716f8c422065258ba7f28d722472188c1a35e718
John Crocker <johnniecrocker@yahoo.com>
2014-10-07 14:30:53 -0400

    set variable to equal image url

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,26 +2,39 @@ console.log('The Iron Yard Rocks');
 
 
 var cont = document.getElementById('mainbox');
+var imageurl,
+    item_price,
+    currency,
+    shop,
+    desc;
 
-var pricearray = items.forEach(function(items){
 
-                                          console.log(items)
-                                          console.log(items.results.length)
-
-
-                                        });
-var results = (25)
-
-var imageurl = items.forEach(function(items){
+items.forEach(function(items){
 
                         items.results.forEach(function(results) {
 
+                            console.log(results.price);
+
+                            item_price = price;
+
+
+
                             results.Images.forEach(function (images){
 
-                              console.log(images.url_170x135);
-                              return ('images.url_170x135');
+                            // console.log(images.url_170x135);
 
-                              })
+
+                              imageurl = images.url_170x135;
+
+                            });
+
+                          //  results.Shop.forEach(function (shop){
+
+                            //  console.log(shop.title)
+
+
+                            //})
+
                             })
                           });
 
