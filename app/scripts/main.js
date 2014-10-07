30caf7017a2ae7e1728722dd1a7e7e9bc384725b
John Crocker <johnniecrocker@yahoo.com>
2014-10-07 12:59:11 -0400

    confirmed  image.js queryable

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('The Iron Yard Rocks');
+
+
+var cont = ('#mainbox');
+
+var pricearray = items.forEach(function(items){
+
+                                          console.log(items)
+
+                                        });
