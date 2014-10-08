ce8cb731fb52dd90364e8a8bb2ea18c5d6802dea
John Crocker <johnniecrocker@yahoo.com>
2014-10-08 07:45:35 -0400

    rough horse boxes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,6 +28,8 @@ items.forEach(function(items){
 
                             currency = results.currency_code;
 
+                            console.log(results.title);
+
                             desc = results.title;
 
                             //console.log(results.Shop.title);
@@ -43,7 +45,7 @@ items.forEach(function(items){
 
                             });
 
-                            $('.forsale').append( '<li><div class="item"><img src=' + imageurl + '>' + '<p class="description">' + desc + '</p> <ul class="bottomimg"><li class="leftimg">' + shopname + '</li>' + '<li class="rightimg">' + item_price + '</li></ul><p class="ad">Ad</p><p class="hamburger"><img src="https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/hamburger.png"</p><p class="heart"><img src="https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/heart.png"</p></div></li>' );
+                            $('.forsale').append( '<li><div class="item"><img src=' + imageurl + '>' + '<p class="description">' + desc + '</p> <ul class="bottomimg"><li class="leftimg">' + shopname + '</li>' + '<li class="rightimg"> $' + item_price + '  ' + currency + '</li></ul><p class="ad">Ad</p><p class="hamburger"><img src="https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/hamburger.png"</p><p class="heart"><img src="https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/heart.png"</p></div></li>' );
 
                             //$('.forsale').append('<li>
                               //                       <div class="item">
@@ -65,6 +67,6 @@ items.forEach(function(items){
                       });
 
 
-$('.description').text(desc);
+//$('.description').text(desc);
 
-$('.rightimg').text("$"+item_price + "  " +currency);
+//$('.rightimg').text("$"+item_price + "  " +currency);
